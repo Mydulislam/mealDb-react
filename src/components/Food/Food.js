12451c1c85988c7b43a2cdd424715bd09d8b791b
Mydulislam <mydulislam693852@gmail.com>
2022-10-04 18:46:39 +0600

    modal inserted

--- src/components/Food/Food.js
+++ src/components/Food/Food.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Food.css';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 const Food = ({ food }) => {
-    const { idMeal, strMealThumb, strMeal, strInstructions, strArea } = food
+    const { idMeal, strMealThumb, strMeal, strInstructions, strArea } = food;
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
         <div className='food-area'>
             <div>
@@ -14,6 +19,25 @@ const Food = ({ food }) => {
             <Link to={`/food/${idMeal}`}>
                 <button>Details</button>
             </Link>
+            <div>
+                <Button variant="primary" onClick={handleShow}>
+                    modal
+                </Button>
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <img className='w-25' src={strMealThumb} alt="" />
+                        <p>{strInstructions ? strInstructions.slice(0, 200) + '...' : 'It is product for MealDb'}</p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
         </div>
     );
 };
